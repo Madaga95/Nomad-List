5b3f5120a2b03805ef1abc8311da8f8e3fab73d9
Madaga95 <amathgueye95@gmail.com>
2024-12-17 17:21:58 +0100

    Initial Commit

---
+++ Nomad List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nomad List
+//
+//  Created by Madaga on 17/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nomad List/Nomad_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nomad_ListApp.swift
+//  Nomad List
+//
+//  Created by Madaga on 17/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Nomad_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
